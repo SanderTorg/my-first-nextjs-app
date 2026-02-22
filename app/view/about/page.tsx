970ba43fc7b84b68cb2fab4c7beeba77d940800a
SanderTorg <santorgersen@gmail.com>
2026-02-22 17:57:25 +0100

    styles: adding little styling to about page

--- app/view/about/page.tsx
+++ app/view/about/page.tsx
@@ -1,20 +1,17 @@
-import Link from "next/link";
-
 export default function AboutPage() {
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <h1>Welcome to Dorgy Codes</h1>
-      <p>
+    <section className="about-page">
+      <h1 className="flex justify-center">Welcome to Dorgy Codes</h1>
+      <p className="flex justify-center">
         This is a simple Next.js application that demonstrates the use of React
         Server Components, dynamic routing, and client-side interactivity. It
         serves as a learning project for understanding the core concepts of
         Next.js and how to build modern web applications with it.
       </p>
-      <p>
+      <p className="flex justify-center">
         Feel free to explore the Todos page to see how data fetching and
         client-side interactions work in Next.js!
       </p>
-    </div>
+    </section>
   );
 }
