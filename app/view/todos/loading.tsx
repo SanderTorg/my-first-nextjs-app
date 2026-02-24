3ef8f4c85d340a2c27621538164007780cdeb0f4
SanderTorg <santorgersen@gmail.com>
2026-02-24 11:33:18 +0100

    remade my build to fix the big Letter Bug

---
+++ app/view/todos/loading.tsx
@@ -0,0 +1,10 @@
+("use client");
+
+function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <p className="text-2xl font-semibold">Loading...</p>
+    </div>
+  );
+}
+export default Loading;
--- app/view/todos/page.tsx
+++ app/view/todos/page.tsx
@@ -2,6 +2,7 @@ import { TodoList } from "@/components/todo-list/TodoList";
 import { Todo } from "@/types/todoTypes";
 import { Suspense } from "react";
 import data from "@/lib/data/todos/todoData.json";
+import Loading from "./loading";
 
 export const BASE_URL =
   process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
@@ -11,36 +12,7 @@ export default async function TodosPage() {
 
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        {<TodoList todos={todos} />}
-      </Suspense>
+      <Suspense fallback={<Loading />}>{<TodoList todos={todos} />}</Suspense>
     </>
   );
 }
-
-//
-// ("use client");
-
-// function error() {
-//   return (
-//     <div className="flex h-screen items-center justify-center">
-//       <h1 className="text-2xl font-bold">
-//         An error occurred while fetching the todos.
-//       </h1>
-//     </div>
-//   );
-// }
-
-// export default error;
-//
-
-// ("use client");
-
-// function Loading() {
-//   return (
-//     <div className="flex h-screen items-center justify-center">
-//       <p className="text-2xl font-semibold">Loading...</p>
-//     </div>
-//   );
-// }
-// export default Loading;
